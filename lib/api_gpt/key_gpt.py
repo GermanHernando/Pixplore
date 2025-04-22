e59763000751193232352ac9afcc0c74d5193ac0
germanHernando <hernandoger@hotmail.com>
2025-04-21 21:18:13 -0300

    Se instaló python, se agregaron sus dependencias en carpeta venv, se generó carpeta api_gpt con código para generar los resultados en los buscadores, se renombró carmpeta home_screen por searchoption_screen actualizando dependencias-el proyecto sigue funcionando correctamente

--- lib/api_gpt/key_gpt.py
+++ lib/api_gpt/key_gpt.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key="our_secret_key", # TODO Replace with your actual API key
 )
 
 completion = client.chat.completions.create(
--- lib/api_gpt/replyapi.py
+++ lib/api_gpt/replyapi.py
@@ -3,7 +3,7 @@ import requests
 response = requests.post(
     "https://api.deepai.org/api/text-generator",
     data={'text': 'Escribe un poema sobre la tecnología.'},
-    headers={'api-key': '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'}
+    headers={'api-key': 'OUR_SECRET_KEY'}  # TODO Replace with your actual API key
 )
 
 print(response.json()['output'])
